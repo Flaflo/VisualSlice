19f0c4e0512a763fb666271b2bb1f8e7848e6336
Flaflo <theflaflo@yahoo.de>
2017-02-02 20:18:54 +0100

    Added fps option

--- Core/src/main/java/net/hybridhacker/visualslice/display/Display.java
+++ Core/src/main/java/net/hybridhacker/visualslice/display/Display.java
@@ -1,5 +1,7 @@
 package net.hybridhacker.visualslice.display;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import lombok.Data;
 import net.hybridhacker.visualslice.utils.G2D;
 
@@ -12,10 +14,12 @@ import net.hybridhacker.visualslice.utils.G2D;
 public final class Display extends Thread {
 
     private static final int BUFFERS = 3;
-
+    
     private final String title;
     private final int width, height;
 
+    private final long fps;
+    
     @Override
     public void run() {
         G2D.window(this.getTitle(), this.getWidth(), this.getHeight(), false, true);
@@ -24,8 +28,14 @@ public final class Display extends Thread {
         while (G2D.window().isShowing()) {
             G2D.push();
             G2D.high();
-
+            
             G2D.pop();
+            
+            try {
+                Thread.sleep(1000 / fps);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Display.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
 }
