cb51cfbf7434f151256e4cfc271c71c9e5d11c5d
Cydhra <j-hengstler@web.de>
2017-02-02 19:40:18 +0100

    empty main method

--- Core/src/main/java/net/hybridhacker/visualslice/Main.java
+++ Core/src/main/java/net/hybridhacker/visualslice/Main.java
@@ -1,6 +1,16 @@
 package net.hybridhacker.visualslice;
 
 /**
- *
+ * Application's main class
  */
-public class Main {}
+public class Main {
+    
+    /**
+     * Main method
+     *
+     * @param args terminal arguments
+     */
+    public static void main(final String... args) {
+        
+    }
+}
