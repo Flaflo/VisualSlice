73e686ebea266062ade79ff1f1270e76df7d0cea
Cydhra <j-hengstler@web.de>
2017-02-03 22:33:16 +0100

    Added getters for important analysis objects in musicplayer

--- Core/src/main/java/net/hybridhacker/visualslice/music/MusicPlayer.java
+++ Core/src/main/java/net/hybridhacker/visualslice/music/MusicPlayer.java
@@ -1,7 +1,10 @@
 package net.hybridhacker.visualslice.music;
 
+import ddf.minim.AudioBuffer;
 import ddf.minim.AudioPlayer;
 import ddf.minim.Minim;
+import ddf.minim.analysis.BeatDetect;
+import ddf.minim.analysis.FFT;
 
 import java.io.File;
 
@@ -10,8 +13,8 @@ import java.io.File;
  */
 public class MusicPlayer {
     
-    private final Minim       minim;
-    private       AudioPlayer audioPlayer;
+    private final Minim minim;
+    private AudioPlayer audioPlayer;
     
     /**
      * Initialize a new audio player
@@ -20,6 +23,11 @@ public class MusicPlayer {
         this.minim = new Minim();
     }
     
+    /**
+     * Load and play an audio file
+     *
+     * @param audioFile audio file
+     */
     public void play(final File audioFile) {
         if (this.audioPlayer.isPlaying()) {
             this.audioPlayer.close();
@@ -27,5 +35,48 @@ public class MusicPlayer {
         
         this.audioPlayer = minim.loadFile(audioFile.getAbsolutePath());
         this.audioPlayer.play();
+    }
+    
+    /**
+     * Stop and close the audio player
+     */
+    public void stop() {
+        this.audioPlayer.close();
+    }
+    
+    /**
+     * @return the right audio buffer
+     */
+    public AudioBuffer getRightAudioBuffer() {
+        return this.audioPlayer.right;
+    }
+    
+    /**
+     * @return the left audio buffer
+     */
+    public AudioBuffer getLeftAudioBuffer() {
+        return this.audioPlayer.left;
+    }
+    
+    /**
+     * @return the audio buffer mixed from right and left
+     */
+    public AudioBuffer getMixedAudioBuffer() {
+        return this.audioPlayer.mix;
+    }
+    
+    /**
+     * @return the beat detect object of the current played track
+     */
+    public BeatDetect getBeatDetector() {
+        // TODO
+        return null;
+    }
+    
+    /**
+     * @return the fast fourier transform object of the current played track. The player handles (un-)registering that object
+     */
+    public FFT getFFT() {
+        return null;
     }
 }
