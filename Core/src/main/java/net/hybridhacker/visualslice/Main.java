02f596ef28bcaac5028ab2dd96dd3b1a099ca066
Cydhra <j-hengstler@web.de>
2017-02-03 22:47:45 +0100

    Setup basic rendering in main

--- Core/src/main/java/net/hybridhacker/visualslice/Main.java
+++ Core/src/main/java/net/hybridhacker/visualslice/Main.java
@@ -1,13 +1,19 @@
 package net.hybridhacker.visualslice;
 
-import java.awt.Color;
 import net.hybridhacker.visualslice.display.Display;
+import net.hybridhacker.visualslice.music.MusicPlayer;
+import net.hybridhacker.visualslice.renderer.VisualizerRenderer;
 import net.hybridhacker.visualslice.utils.G2D;
+import net.hybridhacker.visualslice.visualizer.DebugVisualizer;
+
+import java.awt.*;
 
 /**
  * Application's main class
  */
 public final class Main {
+    
+    private static final MusicPlayer tempTestPlayer = new MusicPlayer();
     
     /**
      * Main method
@@ -17,6 +23,7 @@ public final class Main {
     public static void main(final String... args) {
         final Display display = new Display("VisualSlice", 800, 600);
         display.addRenderer(() -> G2D.clear(Color.BLACK));
+        display.addRenderer(new VisualizerRenderer(new DebugVisualizer(), tempTestPlayer));
         display.start();
     }
 }
