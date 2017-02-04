31fb7d71f63597c722ce62cc3b8682ef7a881f71
Flaflo <theflaflo@yahoo.de>
2017-02-04 12:05:56 +0100

    Added test volume visualizer

--- Core/src/main/java/net/hybridhacker/visualslice/visualizer/DebugVisualizer.java
+++ Core/src/main/java/net/hybridhacker/visualslice/visualizer/DebugVisualizer.java
@@ -3,18 +3,33 @@ package net.hybridhacker.visualslice.visualizer;
 import ddf.minim.AudioBuffer;
 import ddf.minim.analysis.BeatDetect;
 import ddf.minim.analysis.FFT;
+import java.awt.Color;
+import net.hybridhacker.visualslice.utils.ColorUtil;
+import net.hybridhacker.visualslice.utils.G2D;
 
 /**
  * A visualizer for debugging purpose
  */
 public class DebugVisualizer implements IVisualizer {
-    
+
+    private Color[] rainbow;
+
     @Override
-    public void onDraw(final AudioBuffer leftAudioBuffer, final AudioBuffer rightAudioBuffer, final AudioBuffer mixAudioBuffer,
-                       final BeatDetect beatDetect, final FFT fft) {
+    public void onDraw(final int playerLength, final int playerPosition, final AudioBuffer leftAudioBuffer, final AudioBuffer rightAudioBuffer, final AudioBuffer mixAudioBuffer,
+            final BeatDetect beatDetect, final FFT fft) {
+
+        if (rainbow == null) {
+            rainbow = ColorUtil.generateRainbow(playerLength, false, false, false);
+        }
         
+        final int bufferSize = mixAudioBuffer.size() - 1;
+
+        for (int i = 0; i < bufferSize; i++) {
+            G2D.line(i, (int) (50 - leftAudioBuffer.get(i) * 20), i, (int) (50 - leftAudioBuffer.get(i + 1) * 20), rainbow[playerPosition]);
+            G2D.line(i, (int) (50 + rightAudioBuffer.get(i) * 20), i, (int) (50 + rightAudioBuffer.get(i + 1) * 20), rainbow[playerPosition]);
+        }
     }
-    
+
     @Override
     public String getName() {
         return "Debug Visualizer";
