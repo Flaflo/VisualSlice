80a45fb2e1f46dbc05f9e493899b611ac9ff288e
Cydhra <j-hengstler@web.de>
2017-02-04 21:37:36 +0100

    fixed missing element exception in fft setup

--- Core/src/main/java/net/hybridhacker/visualslice/renderer/VisualizerRenderer.java
+++ Core/src/main/java/net/hybridhacker/visualslice/renderer/VisualizerRenderer.java
@@ -4,27 +4,32 @@ import lombok.RequiredArgsConstructor;
 import net.hybridhacker.visualslice.music.IMusicPlayer;
 import net.hybridhacker.visualslice.visualizer.IVisualizer;
 
+import java.util.NoSuchElementException;
+
 /**
  *
  */
 @RequiredArgsConstructor
 public class VisualizerRenderer implements Runnable {
-
+    
     private final IVisualizer visualizer;
     private final IMusicPlayer musicPlayer;
-
+    
     @Override
     public void run() {
-        if (!musicPlayer.getMixedAudioBuffer().isPresent() || !musicPlayer.getLeftAudioBuffer().isPresent()
-                || !musicPlayer.getRightAudioBuffer().isPresent() || !musicPlayer.getLength().isPresent()
-                || !musicPlayer.getPosition().isPresent() && !musicPlayer.getBeatDetect().isPresent() && !musicPlayer.getFFT().isPresent()) {
+        if (!musicPlayer.getMixedAudioBuffer().isPresent() || !musicPlayer.getLeftAudioBuffer().isPresent() ||
+            !musicPlayer.getRightAudioBuffer().isPresent() || !musicPlayer.getLength().isPresent() ||
+            !musicPlayer.getPosition().isPresent() && !musicPlayer.getBeatDetect().isPresent() && !musicPlayer.getFFT().isPresent()) {
             return;
         }
-
+        
         //Forward fft to mixed audio buffer
-        this.musicPlayer.getFFT().get().forward(this.musicPlayer.getMixedAudioBuffer().get());
-
-        this.visualizer.onDraw(musicPlayer.getLength().get(), musicPlayer.getPosition().get(), musicPlayer.getLeftAudioBuffer().get(), musicPlayer.getRightAudioBuffer().get(),
-                musicPlayer.getMixedAudioBuffer().get(), musicPlayer.getBeatDetect().get(), musicPlayer.getFFT().get());
+        try {
+            this.musicPlayer.getFFT().get().forward(this.musicPlayer.getMixedAudioBuffer().get());
+        } catch (NoSuchElementException ignored) {}
+        
+        this.visualizer.onDraw(musicPlayer.getLength().get(), musicPlayer.getPosition().get(), musicPlayer.getLeftAudioBuffer().get(),
+                               musicPlayer.getRightAudioBuffer().get(), musicPlayer.getMixedAudioBuffer().get(),
+                               musicPlayer.getBeatDetect().get(), musicPlayer.getFFT().get());
     }
 }
