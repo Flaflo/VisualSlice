92215b349eefc4082791f487166a8ad11b34ac50
Cydhra <j-hengstler@web.de>
2017-02-05 02:02:20 +0100

    Added example plugin

---
+++ TestPlugin/src/main/java/net/hybridhacker/visualslice/plugin/ExamplePlugin.java
@@ -0,0 +1,30 @@
+package net.hybridhacker.visualslice.plugin;
+
+import net.hybridhacker.visualslice.plugins.AutoRegister;
+import net.hybridhacker.visualslice.plugins.SlicePlugin;
+
+/**
+ * An example plugin
+ */
+@AutoRegister
+public class ExamplePlugin implements SlicePlugin {
+    @Override
+    public void onEnable() {
+        
+    }
+    
+    @Override
+    public void onDisable() {
+        
+    }
+    
+    @Override
+    public String getAuthor() {
+        return "Cydhra";
+    }
+    
+    @Override
+    public String getVersion() {
+        return "1.0";
+    }
+}
