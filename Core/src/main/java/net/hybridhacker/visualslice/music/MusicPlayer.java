1c7a4b33b047835dd42735d2af45f1d33b23ab8a
Cydhra <j-hengstler@web.de>
2017-02-05 14:02:26 +0100

    Re-introduced player's javadoc

--- Core/src/main/java/net/hybridhacker/visualslice/music/MusicPlayer.java
+++ Core/src/main/java/net/hybridhacker/visualslice/music/MusicPlayer.java
@@ -57,42 +57,74 @@ public class MusicPlayer implements AudioListener {
         this.audioPlayer.close();
     }
     
+    /**
+     * Pause the current track
+     */
     public void pause() {
         throw new NotImplementedException();
     }
     
+    /**
+     * Set the players volume
+     *
+     * @param volume a float between 0 and 1
+     */
     public void setVolume(final float volume) {
         throw new NotImplementedException();
     }
     
+    /**
+     * @return the player's volume between 0 and 1
+     */
     public float getVolume() {
         throw new NotImplementedException();
     }
     
+    /**
+     * @return the current track's length in ms
+     */
     public Optional<Integer> getLength() {
         return this.audioPlayer != null ? Optional.of(this.audioPlayer.length()) : Optional.empty();
     }
     
+    /**
+     * @return the player's position in track in ms
+     */
     public Optional<Integer> getPosition() {
         return this.audioPlayer != null ? Optional.of(this.audioPlayer.position()) : Optional.empty();
     }
     
+    /**
+     * @return the right audio buffer for the current track
+     */
     public Optional<AudioBuffer> getRightAudioBuffer() {
         return this.audioPlayer != null ? Optional.of(this.audioPlayer.right) : Optional.empty();
     }
     
+    /**
+     * @return the left audio buffer for the current track
+     */
     public Optional<AudioBuffer> getLeftAudioBuffer() {
         return this.audioPlayer != null ? Optional.of(this.audioPlayer.left) : Optional.empty();
     }
     
+    /**
+     * @return the mixed audio buffer for current track
+     */
     public Optional<AudioBuffer> getMixedAudioBuffer() {
         return this.audioPlayer != null ? Optional.of(this.audioPlayer.right) : Optional.empty();
     }
     
+    /**
+     * @return the beat detect object for the current track
+     */
     public Optional<BeatDetect> getBeatDetect() {
         return Optional.ofNullable(this.beatDetect);
     }
     
+    /**
+     * @return the FFT instance for the current track
+     */
     public Optional<FFT> getFFT() {
         return Optional.ofNullable(this.fft);
     }
