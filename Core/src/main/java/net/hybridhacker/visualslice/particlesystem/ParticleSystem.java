37584cf6b02a6307d771009c079b2604289ded8f
Cydhra <j-hengstler@web.de>
2017-02-05 12:56:13 +0100

    Implemented simple particle system

---
+++ Core/src/main/java/net/hybridhacker/visualslice/particlesystem/ParticleSystem.java
@@ -0,0 +1,45 @@
+package net.hybridhacker.visualslice.particlesystem;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * A basic particle system initialized with some consumers that will handle the system logic
+ */
+public class ParticleSystem {
+    
+    private static final int TPS = 60;
+    private static final double TICK_GAP_MS = 1000D / TPS;
+    
+    private int ticksPassed;
+    
+    private final List<Particle> particles = new ArrayList<>();
+    private final List<Consumer<Integer>> systemHandlers = new LinkedList<>();
+    
+    @SafeVarargs
+    public ParticleSystem(final Consumer<Integer>... handlers) {
+        this.systemHandlers.addAll(Arrays.asList(handlers));
+    }
+    
+    /**
+     * Called whenever the particle system shall update. Calculates the ticks that should have happened yet and calls {@link #doTick(int)}
+     * for every tick that was not executed yet but should have been.
+     */
+    public final void tick(final int positionMs) {
+        while (positionMs / TICK_GAP_MS > ticksPassed) {
+            doTick(++ticksPassed);
+        }
+    }
+    
+    /**
+     * Tick the particle system
+     *
+     * @param tick current tick
+     */
+    public void doTick(final int tick) {
+        this.systemHandlers.forEach(handler -> handler.accept(tick));
+    }
+}
