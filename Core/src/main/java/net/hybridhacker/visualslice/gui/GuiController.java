4feab967ffd1936c3eaaa9388cd409859087c3a7
Cydhra <j-hengstler@web.de>
2017-02-08 00:00:08 +0100

    GuiController now uses DisplaySettings for display creation

--- Core/src/main/java/net/hybridhacker/visualslice/gui/GuiController.java
+++ Core/src/main/java/net/hybridhacker/visualslice/gui/GuiController.java
@@ -2,27 +2,49 @@ package net.hybridhacker.visualslice.gui;
 
 import lombok.Getter;
 import net.hybridhacker.visualslice.display.Display;
+import net.hybridhacker.visualslice.gui.settings.DisplaySettings;
 import net.hybridhacker.visualslice.music.MusicPlayer;
 import net.hybridhacker.visualslice.visualizer.builder.GenericBuilder;
 
 /**
- *
  * @author Flaflo
  */
 public final class GuiController {
-
-    private static final GuiController INSTANCE = new GuiController();
-
+    
+    private static final GuiController instance = new GuiController();
+    
     @Getter
     private final GenericBuilder builder = new GenericBuilder();
-
+    
     @Getter
     private final MusicPlayer player = new MusicPlayer();
     
     @Getter
-    private final Display display = new Display("VisualSlice", 400, 400, 120);
+    private final DisplaySettings displaySettings = new DisplaySettings();
+    
+    private Display display = null;
     
     public static GuiController getInstance() {
-        return INSTANCE;
+        return instance;
+    }
+    
+    /**
+     * This class is a singleton and thus handles its creation on its own
+     */
+    private GuiController() {}
+    
+    /**
+     * Returns the display instance controlled by this controller. As the method is invoked first, the display instance is created and
+     * then saved
+     *
+     * @return the display instance controlled by this controller
+     */
+    public Display getDisplay() {
+        if (this.display == null) {
+            this.display = new Display("VisualSlice", this.displaySettings.getWidth(), this.displaySettings.getHeight(),
+                                       this.displaySettings.getFps());
+        }
+        
+        return this.display;
     }
 }
