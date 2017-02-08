9302f97532845b9319331f6584665527db7c9927
Cydhra <j-hengstler@web.de>
2017-02-08 01:17:54 +0100

    Refactored main class and added CLI option for additional plugin folder

--- Core/src/main/java/net/hybridhacker/visualslice/Main.java
+++ Core/src/main/java/net/hybridhacker/visualslice/Main.java
@@ -1,6 +1,5 @@
 package net.hybridhacker.visualslice;
 
-import java.net.URISyntaxException;
 import net.hybridhacker.visualslice.gui.Controller;
 import net.hybridhacker.visualslice.plugins.PluginManager;
 import net.hybridhacker.visualslice.plugins.loader.InternalPluginLoader;
@@ -10,6 +9,8 @@ import org.apache.commons.cli.MissingArgumentException;
 import org.apache.commons.cli.MissingOptionException;
 import org.apache.commons.cli.ParseException;
 
+import java.net.URISyntaxException;
+
 /**
  * Application's main class
  */
@@ -17,42 +18,36 @@ public final class Main {
     
     private static final String PLUGIN_FOLDER = "plugins/";
     
+    private static final CommandLineInterface commandLineInterface;
+    
+    static {
+        commandLineInterface = new CommandLineInterface();
+        
+        // display settings from command line
+        commandLineInterface.addOption("w", "set the GUI width", "width", false,
+                                       width -> Controller.getInstance().getDisplaySettings().setWidth(Integer.valueOf(width.get())));
+        commandLineInterface.addOption("h", "set the GUI height", "height", false,
+                                       height -> Controller.getInstance().getDisplaySettings().setHeight(Integer.valueOf(height.get())));
+        commandLineInterface.addOption("fps", "the frames per second", "fps", false,
+                                       frames -> Controller.getInstance().getDisplaySettings().setFps(Integer.valueOf(frames.get())));
+        
+        commandLineInterface.addOption("plugins", "load an additional plugins folder", "folder", false,
+                                       folder -> PluginManager.getInstance().attachLoader(new PluginFolderLoader(folder.get())));
+    }
+    
     /**
      * Main method
      *
      * @param args terminal arguments
      */
     public static void main(final String... args) throws URISyntaxException, ParseException {
-        PluginManager.getInstance().attachLoader(new InternalPluginLoader());
-        PluginManager.getInstance().attachLoader(new PluginFolderLoader(PLUGIN_FOLDER));
-        PluginManager.getInstance().loadPlugins();
-        
-        final CommandLineInterface commandLineInterface = new CommandLineInterface();
-    
-        // display settings from command line
-    
-        // the formatter is sadly too bad for this
-        // @formatter:off
-        commandLineInterface.addOption("w", "set the GUI width", "width", false,
-                                       width -> Controller.getInstance().getDisplaySettings().setWidth(Integer.valueOf(
-                                               width.orElse("" + Controller.getInstance().getDisplaySettings().getWidth()))));
-        // @formatter:on
-    
-        commandLineInterface.addOption("h", "set the GUI height", "height", false,
-                                       height -> Controller.getInstance().getDisplaySettings().setHeight(Integer.valueOf(
-                                               height.orElse("" + Controller.getInstance().getDisplaySettings().getHeight()))));
-    
-        commandLineInterface.addOption("fps", "the frames per second", "fps", false,
-                                       frames -> Controller.getInstance().getDisplaySettings().setFps(Integer.valueOf(
-                                               frames.orElse("" + Controller.getInstance().getDisplaySettings().getFps()))));
-        
         // shortcut for help page printing
         if (args.length == 1 && args[0].equalsIgnoreCase("--help")) {
             commandLineInterface.printHelp();
             return;
         }
-    
-        // parse command line
+        
+        // parse and handle command line arguments
         try {
             commandLineInterface.parse(args);
         } catch (final MissingOptionException e) {
@@ -66,7 +61,13 @@ public final class Main {
             System.err.println("Try --help for more information");
             return;
         }
-    
+        
+        // load plugins
+        PluginManager.getInstance().attachLoader(new InternalPluginLoader());
+        PluginManager.getInstance().attachLoader(new PluginFolderLoader(PLUGIN_FOLDER));
+        PluginManager.getInstance().loadPlugins();
+        
+        // start display
         Controller.getInstance().getDisplay().start();
     }
 }
