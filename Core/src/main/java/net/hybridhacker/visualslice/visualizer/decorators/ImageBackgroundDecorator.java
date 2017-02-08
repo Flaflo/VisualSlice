f16d68b14984ae26cfcb20b53ec75afeecdbda68
Cydhra <j-hengstler@web.de>
2017-02-08 03:11:49 +0100

    Bugfix: Image URI was null in image background decorator

--- Core/src/main/java/net/hybridhacker/visualslice/visualizer/decorators/ImageBackgroundDecorator.java
+++ Core/src/main/java/net/hybridhacker/visualslice/visualizer/decorators/ImageBackgroundDecorator.java
@@ -3,32 +3,40 @@ package net.hybridhacker.visualslice.visualizer.decorators;
 import ddf.minim.AudioBuffer;
 import ddf.minim.analysis.BeatDetect;
 import ddf.minim.analysis.FFT;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.net.URI;
-import javax.imageio.ImageIO;
 import lombok.Setter;
 import net.hybridhacker.visualslice.gui.Controller;
 import net.hybridhacker.visualslice.visualizer.AbstractVisualizerDecorator;
 import net.hybridhacker.visualslice.visualizer.IVisualizer;
 import net.hybridhacker.visualslice.visualizer.settings.Setting;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 /**
  * Draws an image as background for a visualizer
  */
 public class ImageBackgroundDecorator extends AbstractVisualizerDecorator {
-
+    
     @Setter
     private URI imageUri;
-
+    
     private BufferedImage image;
-
+    
     public ImageBackgroundDecorator(final IVisualizer visualizer) {
         super(visualizer);
+        
+        try {
+            imageUri = new URI("");
+        } catch (URISyntaxException e) {
+            assert false;
+        }
     }
-
-    private final Setting<?>[] settings = new Setting<?>[]{new Setting<>("Image URI", this.imageUri, this::setImageUri)};
-
+    
+    private final Setting<?>[] settings = new Setting<?>[] {new Setting<>("Image URI", this.imageUri, this::setImageUri)};
+    
     @Override
     protected void doInitialize() {
         try {
@@ -37,25 +45,26 @@ public class ImageBackgroundDecorator extends AbstractVisualizerDecorator {
             e.printStackTrace();
         }
     }
-
+    
     @Override
     protected void doDraw(final AudioBuffer leftAudioBuffer, final AudioBuffer rightAudioBuffer, final AudioBuffer mixAudioBuffer,
-            final BeatDetect beatDetect, final FFT fft, final int trackLength, final int trackPosition) {
+                          final BeatDetect beatDetect, final FFT fft, final int trackLength, final int trackPosition) {
         if (this.image != null) {
-            Controller.getInstance().getRenderEngine().drawImage(this.image, 0, 0, Controller.getInstance().getDisplay().getWidth(), Controller.getInstance().getDisplay().getHeight());
+            Controller.getInstance().getRenderEngine().drawImage(this.image, 0, 0, Controller.getInstance().getDisplay().getWidth(),
+                                                                 Controller.getInstance().getDisplay().getHeight());
         }
     }
-
+    
     @Override
     public AbstractVisualizerDecorator create(final IVisualizer embeddedVisualizer) {
         return new ImageBackgroundDecorator(embeddedVisualizer);
     }
-
+    
     @Override
     public String getName() {
         return "Image Background Decorator";
     }
-
+    
     @Override
     public Setting<?>[] getSettings() {
         return this.settings;
